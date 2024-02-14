01d0cc00c57d243654bc2dc95bdf26c0707fc1d6
GGANGTONG <qkrds0914@gmail.com>
2024-02-15 02:15:35 +0900

    국밥

--- src/routes/naver.router.js
+++ src/routes/naver.router.js
@@ -4,19 +4,26 @@ import { isNotLoggedIn } from "../middlewares/login.middleware.js";
 
 const router = express.Router();
 
-router.get("/naver", isNotLoggedIn, passport.authenticate("naver", null), function (req, res) {
-  // @todo Additional handler is necessary. Remove?
-  console.log("/auth/naver failed, stopped");
-});
+router.get(
+  "/naver",
+  isNotLoggedIn,
+  passport.authenticate("naver", null),
+  function (req, res) {
+    // @todo Additional handler is necessary. Remove?
+    console.log("/auth/naver failed, stopped");
+  }
+);
 
 router.get(
-  "/naver/callback", isNotLoggedIn,
+  "/naver/callback",
+  isNotLoggedIn,
   passport.authenticate("naver", {
     failureRedirect: "/auth/naver",
   }),
   function (req, res) {
-    res.redirect("/main");
-    return res.status(200).json({message:"로그인에 성공했습니다."})
+    console.log("s4 - naver.router 1");
+    return res.status(200).redirect("/main");
+    // return res.status(200).json({ message: "로그인에 성공했습니다." });
   }
 );
 
