89074935876625bdf09e7229a238fca5cca4082e
lyukidon <qgx99098@gmail.com>
2024-02-14 15:48:16 +0900

    [feat] 로그인 상태인 경우, 메인 페이지로 redirect & 로그아웃 상태인 경우, 로그인 페이지로 redirect

--- src/middlewares/login.middleware.js
+++ src/middlewares/login.middleware.js
@@ -2,6 +2,7 @@ export const isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
   } else {
+    res.redirect("/login")
     res.status(403).json({ message: "로그인이 필요합니다." });
   }
 };
@@ -11,6 +12,7 @@ export const isNotLoggedIn = (req, res, next) => {
     next();
   } else {
     const message = encodeURIComponent("로그인한 상태입니다.");
-    res.redirect(`/?error=${message}`);
+    res.redirect("/")
+    // res.redirect(`/?error=${message}`);
   }
 };
